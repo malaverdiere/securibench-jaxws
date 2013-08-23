defe8c449d5c79d30b704f4b35b158f01ed79a7c
Marc-André Laverdière <marc-andre@atc.tcs.com>
2013-08-23 13:50:40 -0400

    Slashed some cases, made it compile

---
+++ src/main/java/org/jboss/ws/api/handler/GenericLogicalHandler.java
@@ -0,0 +1,11 @@
+package org.jboss.ws.api.handler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marc-andre
+ * Date: 8/22/13
+ * Time: 3:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GenericLogicalHandler {
+}
---
+++ src/main/java/org/jboss/wsf/test/JBossWSTestHelper.java
@@ -0,0 +1,11 @@
+package org.jboss.wsf.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marc-andre
+ * Date: 8/22/13
+ * Time: 2:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JBossWSTestHelper {
+}
